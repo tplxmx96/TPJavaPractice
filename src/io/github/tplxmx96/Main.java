bbbab9f1a14c8c21cb6032642627131ac44d7b60
tplxmx96 <tplxmx96@gmail.com>
2018-06-03 13:50:13 +0800

    HashMap简单测试

--- src/io/github/tplxmx96/Main.java
+++ src/io/github/tplxmx96/Main.java
@@ -1,12 +1,19 @@
 package io.github.tplxmx96;
 
+import io.github.tplxmx96.collection.MapTest;
 import io.github.tplxmx96.datatype.TypeTest;
 
 public class Main {
     public static void main(String[] args){
         System.out.println("hello world");
 
+        //测试基础数据类型
         TypeTest typeTest = new TypeTest(1,true,.2f,22.2,22l,"测试数据类型");
         System.out.println(typeTest.toString());
+
+        //测试键值对（key-value）的map
+        MapTest mapTest = new MapTest();
+        mapTest.testAdd().iteratorPrint();
+
     }
 }
--- src/io/github/tplxmx96/collection/MapTest.java
+++ src/io/github/tplxmx96/collection/MapTest.java
@@ -1,5 +1,31 @@
 package io.github.tplxmx96.collection;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class MapTest {
+    private Map<String,Object> data = new HashMap<>();
+
+    public MapTest testAdd(){
+        data.put("first_name","tan");
+        data.put("last_name","pan");
+        data.put("address","hunan");
+        data.put("email","tplxmx96@gmail.com");
+        return this;
+    }
+
+    /**
+     * 使用迭代器的方式遍历循环输出key-value
+     */
+    public void iteratorPrint(){
+        Iterator<Map.Entry<String,Object>> it = data.entrySet().iterator();
+        while (it.hasNext()){
+            Map.Entry<String,Object> entry = it.next();
+            System.out.println("key=" + entry.getKey() + " and value=" + entry.getValue());
+        }
+
+    }
+
 
 }
