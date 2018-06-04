21dd1d69d358008efab34bc2feb44f17e4110ca4
tplxmx96 <tplxmx96@gmail.com>
2018-06-04 22:26:19 +0800

    Java构造方法

--- src/io/github/tplxmx96/collection/CircleTest.java
+++ src/io/github/tplxmx96/collection/CircleTest.java
@@ -1,13 +1,23 @@
 package io.github.tplxmx96.collection;
 
 import io.github.tplxmx96.Javaobjectexercise.Circle;
+import io.github.tplxmx96.Javaobjectexercise.Circle2;
 
 public class CircleTest {
     public static void main(String[] args){
-        Circle circle = new Circle();
-        circle.inputRadius();
-        circle.showPreimeter();
-        circle.showArea();
+//        Circle circle = new Circle();
+//        circle.inputRadius();
+//        circle.showPreimeter();
+//        circle.showArea();
+        Circle2 circle2 = new Circle2();
+        circle2.showArea();
+
+
+
+
+
+
+
     }
 
 }
