c0304369d4b9e7c14c7aa21c8612cc0a35c5ea66
tplxmx96 <39433807+tplxmx96@users.noreply.github.com>
2018-06-07 16:28:34 +0800

    Update Book.java

--- src/io/github/tplxmx96/Javaobjectexercise/Book.java
+++ src/io/github/tplxmx96/Javaobjectexercise/Book.java
@@ -1,15 +1,49 @@
-package io.github.tplxmx96.datatype;
+package io.github.tplxmx96.bean;
 
-public class Swap {
-    public static void main(String[] args){
-        int[] nums = {1,2};
-        Swap1(nums);
-        System.out.println(nums[0] + "\t" + nums[1]);
+import java.util.Date;
+
+public class Book {
+//    private String chinese;
+//    private String math;
+//    private String history;
+//    private String geography;
+//    private String political;
+//    private String skill;
+
+    private String bookType;
+    private String bookName;
+    private String startAt;    //借书时间
+    private String endAt;      //借书类别
+
+    public String getBookType() {
+        return bookType;
     }
-    public static void Swap1(int nums[]){
-        int temp = nums[0];
-        nums[0] = nums[1];
-        nums[1] = temp;
 
+    public void setBookType(String bookType) {
+        this.bookType = bookType;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getStartAt() {
+        return startAt;
+    }
+
+    public void setStartAt(String startAt) {
+        this.startAt = startAt;
+    }
+
+    public String getEndAt() {
+        return endAt;
+    }
+
+    public void setEndAt(String endAt) {
+        this.endAt = endAt;
     }
 }
