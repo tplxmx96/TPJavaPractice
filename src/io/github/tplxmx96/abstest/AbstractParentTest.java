f6d9ce9fe2a4d140d1113257d0e8bd1ee4d61726
tplxmx96 <tplxmx96@gmail.com>
2018-06-09 20:40:31 +0800

    Java的抽象练习

--- src/io/github/tplxmx96/abstest/AbstractParentTest.java
+++ src/io/github/tplxmx96/abstest/AbstractParentTest.java
@@ -1,4 +1,17 @@
 package io.github.tplxmx96.abstest;
 
-public class AbstractParentTest {
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+public abstract class AbstractParentTest {
+
+    abstract public void testAbstract();
+
+    abstract public String testAbstract2();
+
+    abstract public int testAbstract3();
+
+    public void testNormal(){
+
+    }
+
 }
--- src/io/github/tplxmx96/abstest/AbstractSub.java
+++ src/io/github/tplxmx96/abstest/AbstractSub.java
@@ -1,4 +1,21 @@
 package io.github.tplxmx96.abstest;
 
-public class AbstractSub {
+public class AbstractSub extends AbstractParentTest{
+
+    @Override
+    public void testAbstract() {
+
+    }
+
+    @Override
+    public String testAbstract2() {
+        return null;
+    }
+
+    @Override
+    public int testAbstract3() {
+        return 0;
+    }
+
+
 }
