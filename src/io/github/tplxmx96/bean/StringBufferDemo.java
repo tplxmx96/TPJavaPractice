94afc4dc7f894ee5071ebbee47bc4499ab48cc02
tplxmx96 <tplxmx96@gmail.com>
2018-06-10 22:30:15 +0800

    Java的字符拼接练习

--- src/io/github/tplxmx96/bean/StringBufferDemo.java
+++ src/io/github/tplxmx96/bean/StringBufferDemo.java
@@ -5,12 +5,44 @@ package io.github.tplxmx96.bean;
  */
 public class StringBufferDemo {
     public static void main(String[] args){
-        String str1 = "a";
-        String str2 = "b";
-        String str3 = str1 + str2;
-        str3 = str1.concat(str2);
+        final int N = 100000;   //10万
+        long starTime = System.currentTimeMillis();
+        String str = "*";
+        for (int i = 0; i < N; i++) { //10万次循环
+            str += "*";
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("+=用时：" + (endTime - starTime) + "毫秒");
 
-        //底层实现;
-        String str4 = new StringBuffer(String.valueOf(str1)).append(str2).toString();
+        //使用StringBuffer进行字符串拼接
+        starTime = System.currentTimeMillis();
+        StringBuffer str1 = new StringBuffer("*");
+        for (int i = 0; i < N; i++) { //10万次循环
+            str1.append("*");
+        }
+
+        endTime = System.currentTimeMillis();
+        System.out.println("StringBuffer的append用时：" + (endTime - starTime) + "毫秒");
+
+
+        //StringBuffer
+        starTime = System.currentTimeMillis();
+        StringBuilder str2 = new StringBuilder("*");
+        for (int i = 0; i < N; i++) { //10万次循环
+            str2.append("*");
+        }
+
+        endTime = System.currentTimeMillis();
+        System.out.println("StringBuilder的append用时：" + (endTime - starTime) + "毫秒");
+
+
+
+//        String str1 = "a";
+//        String str2 = "b";
+//        String str3 = str1 + str2;
+//        str3 = str1.concat(str2);
+//
+//        //底层实现;
+//        String str4 = new StringBuffer(String.valueOf(str1)).append(str2).toString();
     }
 }
