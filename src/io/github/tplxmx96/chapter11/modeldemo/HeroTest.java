e1e01e83c2601d525a570a3b2fb3098b878136d6
tplxmx96 <tplxmx96@gmail.com>
2018-06-16 15:51:52 +0800

    test

--- src/io/github/tplxmx96/chapter11/modeldemo/HeroTest.java
+++ src/io/github/tplxmx96/chapter11/modeldemo/HeroTest.java
@@ -2,10 +2,10 @@ package io.github.tplxmx96.chapter11.modeldemo;
 
 public class HeroTest {
     public static void main(String[] args){
-        //两个战士之间的对战
-        Hero hero1 = new Warrior(1,"盖伦",0,0);
-        Hero hero2 = new Warrior(2,"貂蝉",50,50);
-        hero1.PK(hero2);
+//        //两个战士之间的对战
+//        Hero hero1 = new Warrior(1,"盖伦",0,0);
+//        Hero hero2 = new Warrior(2,"貂蝉",50,50);
+//        hero1.PK(hero2);
 
 
 
