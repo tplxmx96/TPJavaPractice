5a17239f9b330799f2dcde65ef5d798659a9a0f2
tplxmx96 <tplxmx96@gmail.com>
2018-06-17 16:04:47 +0800

    Java的免检异常示范

---
+++ src/io/github/tplxmx96/chapter12/UnCheckedException.java
@@ -0,0 +1,13 @@
+package io.github.tplxmx96.chapter12;
+
+/**
+ * 免检异常 - RunTimeException
+ */
+public class UnCheckedException {
+
+
+    public static void main(String[] args){
+        String value = null;
+        System.err.println(value.toString());
+    }
+}
