8b37b1dc24c7be28906b5bdbf6597f470f2ff95b
tplxmx96 <tplxmx96@gmail.com>
2018-06-17 16:14:49 +0800

    Java的必检异常示范

--- src/io/github/tplxmx96/chapter12/CheckedException.java
+++ src/io/github/tplxmx96/chapter12/CheckedException.java
@@ -1,4 +1,21 @@
 package io.github.tplxmx96.chapter12;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * 必检异常 - 除了RunTimeException和Error的其他类的子类
+ */
 public class CheckedException {
+    public static void main(String[] args){
+        File file = new File("c:/test.txt");
+        try {
+            FileWriter writer = new FileWriter(file);//文件的写入器，用来将内容写入到文件中
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //判断这个文件是否存在
+        System.out.println(file.exists());
+    }
 }
