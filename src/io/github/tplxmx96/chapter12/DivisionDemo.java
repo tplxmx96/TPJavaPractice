51bacbc51fa2f44654856c2ece2e301f36bfacb2
tplxmx96 <tplxmx96@gmail.com>
2018-06-19 17:36:49 +0800

    Java的方法抛出异常练习

--- src/io/github/tplxmx96/chapter12/DivisionDemo.java
+++ src/io/github/tplxmx96/chapter12/DivisionDemo.java
@@ -1,12 +1,14 @@
 package io.github.tplxmx96.chapter12;
 
+import java.util.InputMismatchException;
+
 public class DivisionDemo {
     /**
      * 计算两个字符串数字的除法(需要类型转换)
      * @param strName1
      * @param strName2
      */
-    public static void division(String strName1, String strName2) throws Exception{
+    public static void division(String strName1, String strName2) throws ArithmeticException,NumberFormatException {
         int num1 = Integer.parseInt(strName1);
         int num2 = Integer.parseInt(strName2);
         int result = num1 / num2;
@@ -16,10 +18,10 @@ public class DivisionDemo {
     public static void main(String[] args){
         try {
             //谁调用，谁处理
-            division("123","abc");
-        } catch (Exception e) {
+            division("123","0");
+        } catch (ArithmeticException | NumberFormatException e) {
             e.printStackTrace();
         }
-        System.out.println("程序结束！");
+        System.out.println("程序正常结束！");
     }
 }
