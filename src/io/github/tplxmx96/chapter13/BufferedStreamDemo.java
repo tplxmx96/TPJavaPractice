9bbb83a0842c476ed1a270d60f0aa2f57660b181
tplxmx96 <tplxmx96@gmail.com>
2018-06-22 22:20:57 +0800

    Java的缓存的输入流练习

--- src/io/github/tplxmx96/chapter13/BufferedStreamDemo.java
+++ src/io/github/tplxmx96/chapter13/BufferedStreamDemo.java
@@ -1,21 +1,19 @@
 package io.github.tplxmx96.chapter13;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * 使用带缓冲的流增加读写效率
  */
 public class BufferedStreamDemo {
-    static final String FilePath = "src/io/github/tplxmx96/chapter13/FileStreamDemo.java";
+    static final String FilePath1 = "src/io/github/tplxmx96/chapter13/FileDemo.java";
+    static final String FilePath2 = "src/io/github/tplxmx96/chapter13/Buffter.txt";
 
     public static void main(String[] args){
         FileInputStream fInputStream = null;
         BufferedInputStream bInputStream = null;
         try {
-            fInputStream = new FileInputStream(FilePath);
+            fInputStream = new FileInputStream(FilePath1);
             bInputStream = new BufferedInputStream(fInputStream);
             byte[] bytes = new byte[bInputStream.available()];
             bInputStream.read(bytes);
@@ -28,6 +26,22 @@ public class BufferedStreamDemo {
             try {
                 bInputStream.close();
                 fInputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        BufferedOutputStream bOutStream = null;
+        try {
+            bOutStream = new BufferedOutputStream(new FileOutputStream(FilePath2),102400);
+            bOutStream.write("今天天气很好！".getBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                bOutStream.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
