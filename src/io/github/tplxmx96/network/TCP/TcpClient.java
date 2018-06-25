99b52fed35097ba4290f653e532a0675683eb499
tplxmx96 <tplxmx96@gmail.com>
2018-06-25 23:01:19 +0800

    Java的TCP客服端服务端练习

--- src/io/github/tplxmx96/network/TCP/TcpClient.java
+++ src/io/github/tplxmx96/network/TCP/TcpClient.java
@@ -1,4 +1,35 @@
 package io.github.tplxmx96.network.TCP;
 
+/**
+ * 演示tcp传输
+ * 1、tcp分客服端对应的对象是Socket.
+ * 2、服务端对应的对象是Socket
+ */
+
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * 客服端
+ * 通过查阅socket对象，发现在该对象建立时，就可以去连接指定主机。
+ * 因为tcp是面向连接的，使用在建立socket服务时，
+ * 就要有服务器存在，并连接成功，形成通落后，在该通道进行数据的传输。
+ *
+ * 需求：给服务端发送一个文本数据
+ * 步骤
+ * 1、创建socket服务，并指定要连接的主机和端口。
+ */
 public class TcpClient {
+    public static void main(String[] args) throws Exception {
+        //创建客服端的socket服务，指定目的的主机和端口
+        Socket s = new Socket("192.168.5.83",10004);
+
+        //为了发送数据，应该获取socket流中的输出流
+        OutputStream out = s.getOutputStream();
+
+        out.write("tcp ge men lai le".getBytes());
+
+        s.close();
+
+    }
 }
--- src/io/github/tplxmx96/network/TCP/TcpServer.java
+++ src/io/github/tplxmx96/network/TCP/TcpServer.java
@@ -1,5 +1,9 @@
 package io.github.tplxmx96.network.TCP;
 
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * 需求：定义端点接收数据并打印在控制台上
  *
@@ -10,10 +14,32 @@ package io.github.tplxmx96.network.TCP;
  * 通过ServerSocket的accept方法，没有连接就会等，所以这个方法是阻塞式的。
  * 3、客服端如果发过来数据，那么服务端要使用对应的客服端对象，并获取到该客服端对象的读取流来读取发过来的数据。
  * 并打印在控制台。
- * 4、关闭服务端。
+ * 4、关闭服务端。(可选)
  */
-public class TcpDemo {
-    public static void main(String[] args){
+public class TcpServer {
+    public static void main(String[] args)throws Exception{
+        //建立服务端socket服务，并监听一个端口。
+        ServerSocket ss = new ServerSocket(10004);
+
+        //通过accept方法获取连接过来的客服端对象。
+        Socket s = ss.accept();
+
+        String ip = s.getInetAddress().getHostAddress();
+        System.out.println(ip+".....connected");
+
+        //获取客服端发送过来的数据，那么要使用客服端对象的读取流来读取数据。
+        InputStream in = s.getInputStream();
+
+        byte[] buf = new byte[1024];
+        int len = in.read(buf);
+
+        System.out.println(new String(buf,0,len));
+
+        s.close();//关闭客服端
+        ss.close();//关闭服务端
+
+
+
 
     }
 }
