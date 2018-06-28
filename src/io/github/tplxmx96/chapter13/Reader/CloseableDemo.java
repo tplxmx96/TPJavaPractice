0e6c38dfc0897761087b9c096723c093f3d6c2ad
tplxmx96 <tplxmx96@gmail.com>
2018-06-28 16:33:23 +0800

    Java的网络读取文件缓存练习

--- src/io/github/tplxmx96/chapter13/Reader/CloseableDemo.java
+++ src/io/github/tplxmx96/chapter13/Reader/CloseableDemo.java
@@ -1,4 +1,24 @@
 package io.github.tplxmx96.chapter13.Reader;
 
+import java.io.IOException;
+
 public class CloseableDemo {
+//    public static void main(String[] args){
+//        try(SomeResrouce some =new SomeResrouce()){
+//            throw new IOException("手动抛出");
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
+//    }
+}
+
+/**
+ * 我们自定义的模拟资源类(有可能是连接、文件流）
+ * 实现Closeable接口，以便让try()自动进行关闭资源
+ */
+class SomeResrouce implements Cloneable{
+    public void close() throws IOException{
+        System.out.println("打开的SomeResrouce资源已被关闭！");
+
+    }
 }
--- src/io/github/tplxmx96/chapter13/Reader/ReadURLContent.java
+++ src/io/github/tplxmx96/chapter13/Reader/ReadURLContent.java
@@ -10,14 +10,18 @@ import java.net.URL;
  * 从给定的url上读取文件内容 - 暂时只支持http协议
  */
 public class ReadURLContent {
+    static final String FilePath = "src/io/github/tplxmx96/chapter13/Reader/163.txt";
     public static void main(String[] args) throws MalformedURLException {
         URL url = new URL("http://www.163.com/index.html");
         //将InputStream转换成Reader的套路：使用装饰器类
+        StringBuffer content = new StringBuffer();
         try(BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()))){
             String line = null;
             while ((line = reader.readLine()) != null){
-                System.out.println(line);
+                content.append(line);
+                content.append(System.getProperty("line.separator"));
             }
+            ReplaceLyriceDemo.WriterFileByPrintWriter(content.toString(),FilePath,false);
         } catch (IOException e) {
             e.printStackTrace();
         }
